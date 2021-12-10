a6ad93ec8110414c05490abcb5bbe3917cc0fe76
jahanara khatun <zarakhatun58@gmail.com>
2021-12-10 11:25:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f2bxu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+/*
+KVEgePgemagJxa29
+visume
+*/
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("connect to database");
+    const database = client.db("visume");
+
+    const usersCollection = database.collection("users");
+
+    //post api for email password
+    app.post("/users", async (req, res) => {
+      const users = req.body;
+      const result = await usersCollection.insertOne(users);
+      res.json(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("running to get user in server");
+});
+app.listen(port, () => {
+  console.log("listening on port", port);
+});
